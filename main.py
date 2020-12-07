5a846fed92a8b1a6de7b6be0b0716886c67f12f7
litlprogram <ovramets.i@donnu.edu.ua>
2020-12-07 22:51:54 +0000

    MODUL 4

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 #https://repl.it/join/zzdhbhhl-nesterenkov
-x1=int(input('Введіть число 1:'))
-x2=int(input('Введіть число 2:'))
-x3=int(input('Введіть число 3:'))
-x4=int(input('Введіть число 4:'))
-
-if x1 % 2 == 1 or x2 % 2 == 1:
-  if x3 % 2 == 1 or x4 % 2 == 1:
-    if x1 % 2 == 0 or x2 % 2 == 0:
-      if x3 % 2 == 0 or x4 % 2 == 0:
-        print("YES")
-      else:
-        print("NO")		
+P = float(input("Введіть Процент-->"))
+X = float(input("Введіть рублі-->"))
+z = float(input("Введіть копійки-->"))
+K = float(input("Введіть кількість років-->"))
+Y = z / 100
+prk = (X+Y)
+proc = (X+Y) /100
+vart = proc * P
+vars = vart * K
+prk = (X+Y) + vars
+print("Кінцева сума-->",prk)
