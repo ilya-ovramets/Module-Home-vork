ec7da33d5e25b2c55bcaae464bea1c1873fc81c5
litlprogram <ovramets.i@donnu.edu.ua>
2020-12-16 16:42:32 +0000

    hdgwed

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 #https://repl.it/join/zzdhbhhl-nesterenkov
-P = float(input("Введіть Процент-->"))
-X = float(input("Введіть рублі-->"))
-z = float(input("Введіть копійки-->"))
-K = float(input("Введіть кількість років-->"))
-Y = z / 100
-prk = (X+Y)
-proc = (X+Y) /100
-vart = proc * P
-vars = vart * K
-prk = (X+Y) + vars
-print("Кінцева сума-->",prk)
+from Modul1 import Task2
+
+
+
+  
