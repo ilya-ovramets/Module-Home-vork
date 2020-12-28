e3690a1c4b38f177ebbad7aeb8e4ce7689071788
litlprogram <ovramets.i@donnu.edu.ua>
2020-12-28 15:51:21 +0000

    Modul2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#https://repl.it/join/zzdhbhhl-nesterenkov
-from Modul1 import Task2
+#https://repl.it/@nesterenkov/PythonHomeWork#main.py
+#from Modul1 import Task22
+from Modul2 import Task11
+#from Modul3 import Task9
 
-
-
-  
